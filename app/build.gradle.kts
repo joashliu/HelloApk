1d4b672c7d520979220347ed8a163ca56182a48e
joashliu <joashjoashliu@gmail.com>
2026-09-26 02:18:49 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.hello"
         minSdk = 24
         targetSdk = 34
-        versionCode = 18
-        versionName = "2.7"
+        versionCode = 19
+        versionName = "2.8"
     }
 
     buildTypes {
